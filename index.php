ffb16cc9e96a9225d4c088d65a669b24c94be12a
Alumno <alumno@sistemas-utnfra.com>
2023-05-22 21:20:02 -0300

    commit 1

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $app->addBodyParsingMiddleware();
 
 // Routes
 $app->get('[/]', function (Request $request, Response $response) {    
-    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework 2023"));
+    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a UTNFRA 2023"));
     $response->getBody()->write($payload);
     return $response->withHeader('Content-Type', 'application/json');
 });
